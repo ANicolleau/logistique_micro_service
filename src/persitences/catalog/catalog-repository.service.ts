d37f02d7e802a43be8c465892fc7fd1b8e8d80b5
Benoit <43681702+BenoitYnov@users.noreply.github.com>
2022-03-24 16:42:42 +0100

    Issue 2 - Réception de produit en stock (#9)
    Co-authored-by: Benoit Thrive <benoit@agencethrive.fr>

---
+++ src/persitences/catalog/catalog-repository.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from "@nestjs/common";
+import {ProductDao} from "./catalog.dao";
+import axios from "axios";
+
+@Injectable()
+export class CatalogRepositoryService {
+
+    public async getCatalog(): Promise<ProductDao[]> {
+        const server = 'https://fhemery-logistics.herokuapp.com';
+        const path: string = '/api/products/';
+
+        const result = await axios.get(server + path)
+        //@ts-ignore
+        return result.data
+    }
+
+    public async addProduct(product: ProductDao): Promise<string> {
+        const server = 'https://fhemery-logistics.herokuapp.com';
+        const path: string = '/api/products/';
+
+        const result = await axios.post(server + path, product)
+        //@ts-ignore
+        return result.data
+    }
+
+
+}
--- src/persitences/stock/stock-repository.service.ts
+++ src/persitences/stock/stock-repository.service.ts
@@ -11,4 +11,6 @@ export class StockRepositoryService {
 
         await axios.post(server + path, supplyToAdd)
     }
+
+
 }
