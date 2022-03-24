5f7e0b933ead163cd2155a04be2542c3e0594ad4
Florian Lafuente <33687200+paralixo@users.noreply.github.com>
2022-03-24 16:10:29 +0100

    fix axios - body qui ne s'envoyait pas (#7)
    Co-authored-by: Florian Lafuente <florian.lafuente@gmail.com>

--- src/persitences/stock/stock-repository.service.ts
+++ src/persitences/stock/stock-repository.service.ts
@@ -1,16 +1,14 @@
 import {Injectable} from "@nestjs/common";
 import {SupplyToAddDao} from "./stock.dao";
-import {HttpService} from "@nestjs/axios";
+import axios from "axios";
 
 @Injectable()
 export class StockRepositoryService {
-    constructor(private httpService: HttpService) {}
 
     public async addStock(supplyToAdd: SupplyToAddDao): Promise<void> {
         const server = 'https://archi-logicielle.herokuapp.com';
         const path: string = '/api/stock/' + supplyToAdd.productId + '/movement';
 
-        await this.httpService.post(server + path, {body: supplyToAdd})
-        // TODO: vérifier que la réponse est bien 204 sinon erreur
+        await axios.post(server + path, supplyToAdd)
     }
 }
